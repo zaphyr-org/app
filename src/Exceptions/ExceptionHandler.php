5363e78e38a0b7758dd8da5a558877eb83ea5ecc
merloxx <merloxx@zaphyr.org>
2024-02-23 06:16:56 +0100

    Changed: Added `view` method to ExceptionHandler::renderHtmlView

--- src/Exceptions/ExceptionHandler.php
+++ src/Exceptions/ExceptionHandler.php
@@ -7,9 +7,6 @@ namespace App\Exceptions;
 use Psr\Http\Message\ResponseInterface;
 use Throwable;
 use Zaphyr\Framework\Exceptions\Handlers\ExceptionHandler as BaseExceptionHandler;
-use Zaphyr\Router\Exceptions\MethodNotAllowedException;
-use Zaphyr\Router\Exceptions\NotFoundException;
-use Zaphyr\Utils\Template;
 
 class ExceptionHandler extends BaseExceptionHandler
 {
@@ -18,16 +15,18 @@ class ExceptionHandler extends BaseExceptionHandler
      */
     protected function renderHtmlView(ResponseInterface $response, Throwable $throwable): ResponseInterface
     {
-        $template = match (get_class($throwable)) {
-            NotFoundException::class => '404.html',
-            MethodNotAllowedException::class => '405.html',
-            default => '500.html',
-        };
+        return view($this->application, 'errors/' . $this->determineStatusCode($throwable));
+    }
 
-        $response->getBody()->write(
-            Template::render($this->application->getResourcesPath('views/errors/' . $template))
-        );
+    /**
+     * @param Throwable $throwable
+     *
+     * @return int
+     */
+    protected function determineStatusCode(Throwable $throwable): int
+    {
+        $code = $throwable->getCode();
 
-        return $response;
+        return ($code !== 404 && $code !== 405) ? 500 : $code;
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Zaphyr\Framework\Contracts\ApplicationInterface;
 use Zaphyr\Framework\Http\HtmlResponse;
+use Zaphyr\Utils\Exceptions\UtilsException;
 use Zaphyr\Utils\Template;
 
 if (!function_exists('view')) {
@@ -12,6 +13,7 @@ if (!function_exists('view')) {
      * @param string                $view
      * @param array<string, string> $data
      *
+     * @throws UtilsException if the view could not be rendered
      * @return HtmlResponse
      */
     function view(ApplicationInterface $app, string $view, array $data = []): HtmlResponse
