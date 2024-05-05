c89e6732af12b935a64b7670890e3ac77be6a7f7
merloxx <merloxx@zaphyr.org>
2024-05-05 16:55:47 +0200

    Changed: File structure adapted to new framework conditions

--- public/index.php
+++ public/index.php
@@ -1,12 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
+use Zaphyr\Framework\Contracts\Kernel\HttpKernelInterface;
+use Zaphyr\Framework\Http\Request;
+use Zaphyr\HttpEmitter\Contracts\EmitterInterface;
+
+// Check if maintenance mode is enabled
 if (file_exists($maintenance = __DIR__ . '/maintenance.html')) {
     require $maintenance;
     exit;
 }
 
+// Require Composer autoload
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Get bootstrapped application instance
 $application = require_once __DIR__ . '/../src/bootstrap.php';
-$application->runHttpRequest(Zaphyr\Framework\Http\Request::fromGlobals());
+$container = $application->getContainer();
 
+// Handle HTTP request
+$request = Request::fromGlobals();
+$response = $container->get(HttpKernelInterface::class)->handle($request);
+return $container->get(EmitterInterface::class)->emit($response);
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,40 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Exceptions\ExceptionHandler;
-use Zaphyr\Framework\Application;
 use Zaphyr\Framework\Contracts\Exceptions\Handlers\ExceptionHandlerInterface;
+use Zaphyr\Framework\Contracts\Kernel\ConsoleKernelInterface;
+use Zaphyr\Framework\Contracts\Kernel\HttpKernelInterface;
+use Zaphyr\Framework\Kernel\ConsoleKernel;
+use Zaphyr\Framework\Kernel\HttpKernel;
+use Zaphyr\HttpEmitter\Contracts\EmitterInterface;
+use Zaphyr\HttpEmitter\SapiEmitter;
 
-$application = new Application(dirname(__DIR__), initBindingsOverwrites: [
-    ExceptionHandlerInterface::class => ExceptionHandler::class
-]);
+// Create new application instance
+$application = new Zaphyr\Framework\Application(dirname(__DIR__));
+$container = $application->getContainer();
 
+// Bind important interfaces
+$container->bindSingleton(
+    HttpKernelInterface::class,
+    HttpKernel::class
+);
+
+$container->bindSingleton(
+    ConsoleKernelInterface::class,
+    ConsoleKernel::class
+);
+
+$container->bindSingleton(
+    ExceptionHandlerInterface::class,
+    ExceptionHandler::class
+);
+
+$container->bindSingleton(
+    EmitterInterface::class,
+    SapiEmitter::class
+);
+
+// Return bootstrapped application instance
 return $application;
