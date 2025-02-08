e130b4bd4caae30d1e39a0f7aa029d05a3fd8103
merloxx <merloxx@zaphyr.org>
2025-02-08 09:54:34 +0100

    Changed: Updated application to new framework version requirements

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use App\Exceptions\ExceptionHandler;
 use Zaphyr\Framework\Contracts\Exceptions\Handlers\ExceptionHandlerInterface;
 use Zaphyr\Framework\Contracts\Kernel\ConsoleKernelInterface;
@@ -11,15 +9,26 @@ use Zaphyr\Framework\Kernel\HttpKernel;
 use Zaphyr\HttpEmitter\Contracts\EmitterInterface;
 use Zaphyr\HttpEmitter\SapiEmitter;
 
-// Create new application instance
-$application = new Zaphyr\Framework\Application(dirname(__DIR__));
-$container = $application->getContainer();
+$application = new Zaphyr\Framework\Application();
 
-// Bind important interfaces
-$container->bindSingleton(HttpKernelInterface::class, HttpKernel::class);
-$container->bindSingleton(ConsoleKernelInterface::class, ConsoleKernel::class);
-$container->bindSingleton(ExceptionHandlerInterface::class, ExceptionHandler::class);
-$container->bindSingleton(EmitterInterface::class, SapiEmitter::class);
+/*-------------------------------------------------------
+ * BIND ESSENTIAL INTERFACES
+ *-------------------------------------------------------
+ * To ensure the application functions correctly, key
+ * framework interfaces must be bound to their respective
+ * implementations. These bindings allow the dependency
+ * injection container to resolve the required instances
+ * throughout the application.
+ *
+ * - HttpKernelInterface: Handles incoming HTTP requests.
+ * - ConsoleKernelInterface: Manages console commands.
+ * - ExceptionHandlerInterface: Handles exceptions globally.
+ * - EmitterInterface: Sends HTTP responses to the client.
+ */
+$application->getContainer()
+    ->bindSingleton(HttpKernelInterface::class, HttpKernel::class)
+    ->bindSingleton(ConsoleKernelInterface::class, ConsoleKernel::class)
+    ->bindSingleton(ExceptionHandlerInterface::class, ExceptionHandler::class)
+    ->bindSingleton(EmitterInterface::class, SapiEmitter::class);
 
-// Return bootstrapped application instance
 return $application;
--- public/index.php
+++ public/index.php
@@ -1,25 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
-use Zaphyr\Framework\Contracts\Kernel\HttpKernelInterface;
+use Zaphyr\Framework\Application;
 use Zaphyr\Framework\Http\Request;
-use Zaphyr\HttpEmitter\Contracts\EmitterInterface;
 
-// Check if maintenance mode is enabled
 if (file_exists($maintenance = __DIR__ . '/maintenance.html')) {
     require $maintenance;
     exit;
 }
 
-// Require Composer autoload
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Get bootstrapped application instance
+/** @var Application $application */
 $application = require_once __DIR__ . '/../app/bootstrap.php';
-$container = $application->getContainer();
-
-// Handle HTTP request
-$request = Request::fromGlobals();
-$response = $container->get(HttpKernelInterface::class)->handle($request);
-return $container->get(EmitterInterface::class)->emit($response);
+$application->handleRequest(Request::fromGlobals());
