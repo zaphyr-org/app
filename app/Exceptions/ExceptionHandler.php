18e96f3c59ebbbd592107991927836c9d6528f44
merloxx <merloxx@zaphyr.org>
2025-07-16 17:08:32 +0200

    Changed: Improved renderHtmlView method in ExceptionHandler class

--- app/Exceptions/ExceptionHandler.php
+++ app/Exceptions/ExceptionHandler.php
@@ -7,31 +7,20 @@ namespace App\Exceptions;
 use Psr\Http\Message\ResponseInterface;
 use Throwable;
 use Zaphyr\Framework\Exceptions\Handlers\ExceptionHandler as BaseExceptionHandler;
-use Zaphyr\Utils\Exceptions\UtilsException;
 
 class ExceptionHandler extends BaseExceptionHandler
 {
     /**
      * {@inheritdoc}
-     *
-     * @throws UtilsException if the view file is not found
      */
     protected function renderHtmlView(ResponseInterface $response, Throwable $throwable): ResponseInterface
     {
-        $statusCode = $this->determineStatusCode($throwable);
+        try {
+            $statusCode = $throwable->getCode();
 
-        return view('errors/' . $statusCode, statusCode: $statusCode);
-    }
-
-    /**
-     * @param Throwable $throwable
-     *
-     * @return int
-     */
-    protected function determineStatusCode(Throwable $throwable): int
-    {
-        $code = $throwable->getCode();
-
-        return ($code !== 404 && $code !== 405) ? 500 : $code;
+            return view('errors/' . $statusCode, statusCode: $statusCode);
+        } catch (Throwable) {
+            return parent::renderHtmlView($response, $throwable);
+        }
     }
 }
